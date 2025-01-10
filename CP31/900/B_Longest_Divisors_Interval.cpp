ab00128bf8f18ceb8220a756d55c98ebba442799
Murtaza Narwarwala <murtazanarwar@gmail.com>
2025-01-10 22:10:59 +0530

    10Jan

--- CP31/900/B_Longest_Divisors_Interval.cpp
+++ CP31/900/B_Longest_Divisors_Interval.cpp
@@ -7,7 +7,7 @@ int main(){
     long t; cin>>t;
     while(t--){
         ll n; cin>>n;
-        ll cnt(1);
+        ll cnt = 1;
         for(ll p = 2; n % p == 0 && p <= n; p++){++cnt;}
         cout<<cnt<<'\n';
     }
